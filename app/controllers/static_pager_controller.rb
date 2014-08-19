a790958b92c1cdcf304d23b22f2a5412a58a0003
Dela Yun <delaxxxway@gmail.com>
2014-08-19 14:34:24 +0000

    Add a StaticPager controler

---
+++ app/controllers/static_pager_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagerController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def help
+  end
+end
---
+++ test/helpers/static_pager_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StaticPagerHelperTest < ActionView::TestCase
+end
