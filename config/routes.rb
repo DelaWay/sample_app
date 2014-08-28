30596827744e2d7b566dd5d2adb70e5b3f96e841
Dela Yun <delaxxxway@gmail.com>
2014-08-28 15:09:11 +0000

    Created sessions after signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
  
-  resources :users
-
   root 'static_pager#home'
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   match 'signup', to: 'users#new', via: 'get'
+  match 'signin', to: 'sessions#new', via: 'get'
+  match 'signout', to: 'sessions#destroy', via: 'delete'
   match 'help', to: 'static_pager#help', via: 'get'
   match 'about', to: 'static_pager#about', via: 'get'
-
-
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
