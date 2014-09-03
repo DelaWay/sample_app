5aa93028f75c376cc9c4e387c729c1cda792d5ea
Dela Yun <delaxxxway@gmail.com>
2014-09-03 08:47:47 +0000

    Relationship model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   before_create { self.email = email.downcase }
   before_create :create_remember_token
   
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   
   has_secure_password
   
